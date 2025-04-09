05802222e95c2b77b91b56ea5b75ece984679f5d
Shaon Ahmed <shaon.ahmed@hackberry.se>
2025-04-09 23:40:55 +0200

    Testint implicit and explicit assertions

--- cypress/e2e/assertion-demo.cy.js
+++ cypress/e2e/assertion-demo.cy.js
@@ -1,4 +1,14 @@
 it("Assertion Demo", () => {
   cy.visit("https://example.cypress.io");
   cy.contains("get").click();
+  cy.get("#query-btn")
+    .should("contain", "Button")
+    .and("have.class", "query-btn")
+    .and("be.visible")
+    .and("be.enabled");
+
+  expect(true).to.be.true;
+
+  assert.equal(4, "4", "NOT_EQUAL"); // Will pass because of type coercion
+  assert.strictEqual(4, "4", "EQUAL"); // Will fail strict equality
 });
